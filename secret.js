77f67fcd05c567a05721130a491185016f301bd4
IgnisFrostburn <159628739+IgnisFrostburn@users.noreply.github.com>
2024-02-10 21:32:41 +0800

    Add files via upload

---
+++ secret.js
@@ -0,0 +1,29 @@
+const space = document.querySelector('.space');
+const question = document.querySelector('.question');
+const yes_button = document.querySelector('.yes_button');
+const no_button = document.querySelector('.no_button');
+
+const spaceRect = space.getBoundingClientRect();
+const yesRect = yes_button.getBoundingClientRect();
+const noRect = no_button.getBoundingClientRect();
+
+var counter = 0;
+
+yes_button.addEventListener('click', () => {
+    if(counter == 0) {
+        question.innerHTML = "REALLY?"
+    } else {
+        question.innerHTML = "Then will you be my <span style = 'font-size: 100px; font-family: valentine;'>valentine</span>?"
+        yes_button.innerHTML = "<a style='text-decoration: none; color: white; font-size: 50px' href='./next.html'>Yes</a>"
+    }
+    counter++;
+    
+})
+
+no_button.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (spaceRect.width - noRect.width)) + 1;
+    const j = Math.floor(Math.random() * (spaceRect.height - noRect.height)) + 1;
+
+    no_button.style.left = i + 'px';
+    no_button.style.top = j + 'px';
+})
